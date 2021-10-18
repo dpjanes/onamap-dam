d6585cdf68e9540f492168b75bc1a94a36491836
David Janes <davidjanes@davidjanes.com>
2021-10-18 15:03:27 -0400

    images

--- dam/pei2014/Process.py
+++ dam/pei2014/Process.py
@@ -1,11 +1,33 @@
 import os
+import re
 import sys
 import pprint
 import hashlib
 import base64
+import datetime
+
+import pytz
 import yaml
+from markdownify import markdownify
 
 __folder__ = os.path.dirname(__file__)
+
+TZ_LOCAL = pytz.timezone("America/Halifax")
+TZ_UTC = pytz.timezone("UTC")
+
+def normalize_date(s_local, eod=False):
+    if s_local is None:
+        return 
+
+    if eod:
+        s_local += "T23:59:59"
+
+    dt_local = TZ_LOCAL.localize(datetime.datetime.fromisoformat(s_local))
+    dt_utc = dt_local.astimezone(TZ_UTC)
+    s_utc = dt_utc.isoformat()
+    s_utc = re.sub("[+].*$", "Z", s_utc)
+
+    return s_utc
 
 def hash(s):
     hashed = hashlib.md5(s.encode())
@@ -21,6 +43,17 @@ def denull(d):
             del d[key]
 
     return d
+
+def scrub_content(text):
+    if text is None:
+        return
+
+    text = markdownify(text)
+    text = re.sub("#### Region.*$", "", text, flags=re.DOTALL|re.MULTILINE)
+    text = re.sub("\n+", "\n", text, flags=re.DOTALL|re.MULTILINE)
+    text = text.strip()
+
+    return text
 
 class Processor:
     def __init__(self):
@@ -40,14 +73,28 @@ class Processor:
     def cook_one(self, record):
         record_id = record["id"]
 
+        images = []
+        for photo in record.get("photos", []):
+            image = {
+                "type": "Image",
+                "client_id": hash(photo),
+                "url": photo,
+            }
+            self.add(image)
+            images.append({
+                "type": "Image",
+                "client_id": image["client_id"],
+            })
+
         item = {
             "type": "Item",
             "client_id": hash(f"item-{record_id}"),
             "name": record.get("title"),
-            "description": record.get("content"),
+            "description": scrub_content(record.get("content")),
             "url": record.get("url"),
             "email": record.get("email"),
             "phone": record.get("phone"),
+            "images": images or None,
         }
         self.add(item)
 
@@ -63,14 +110,17 @@ class Processor:
         }
         self.add(location)
 
-        event_start = record.get("event_start")
-        event_end = record.get("event_end")
+        event_start = normalize_date(record.get("event_start"))
+        event_end = normalize_date(record.get("event_end"), eod=True)
         offer = {
             "type": "Offer",
             "client_id": hash(f"offer-{record_id}-{event_start}-{event_end}"),
             "name": record["title"],
-            "date_start": event_start,
-            "date_end": event_end,
+            "period": {
+                "start": event_start,
+                "end": event_end,
+                "all_day": True,
+            },
             "location": {
                 "type": "Location",
                 "client_id": location["client_id"],
@@ -89,15 +139,6 @@ class Processor:
 
         self.dorder.append(client_id)
         self.d[client_id] = denull(item)
-        
-        """
- 'guid': '4823920bcebfd000fe4a0cbeba4f53f0',
- 'id': 20037603,
- 'phone': '902-629-1864',
- 'photos': ['https://www.tourismpei.com/search/assets/images/common/37603-1630413734.jpg'],
- 'theme': ['Events'],
-        """
-
 
 if __name__ == '__main__':
     p = Processor()
